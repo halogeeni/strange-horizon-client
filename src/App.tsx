d539a0f2507a215a7592311410b9c39922b15787
Aleksi Rasio <aleksi.rasio@gmail.com>
2021-08-16 12:05:56 +0300

    Adopt logo component in application

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import * as React from "react";
+import { Logo } from './components/Logo';
 import './App.scss';
 
 const App: React.VoidFunctionComponent = () => (
-  <div className="app-wrapper">
-    <p>Hello world!</p>
-  </div>
+  <>
+    <div className="logo-container">
+      <Logo />
+    </div>
+    <p className="text-contact">Orders or questions? <a href="mailto:strangehorizonrecs@gmail.com">Mail us</a></p>
+  </>
 );
 
 export default App;
